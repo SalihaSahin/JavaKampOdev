02c48c1d0be17c08cec4d38a77d0173a7850e253
SalihaSahin <saliha_sahin_01@hotmail.com>
2021-05-05 18:24:41 +0300

    First commit

---
+++ interfaceAbstractDemo/src/interfaceAbstractDemo/Main.java
@@ -0,0 +1,16 @@
+package interfaceAbstractDemo;
+
+import Abstract.BaseCustomerManager;
+import Adapters.MernisServiceAdapter;
+import Concrete.StarbucksCustomerManager;
+import Entities.Customer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
+		customerManager.save(new Customer(1,"SALİHA","ŞAHİN",2000,"12345"));
+	}
+
+}
